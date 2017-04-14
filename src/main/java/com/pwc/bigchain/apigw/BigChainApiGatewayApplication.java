630fd9e97381ec5882a85f6f3f2c228df0da1c92
Lokesh <lokesh3440@gmail.com>
2017-04-14 22:50:53 +0800

    controller changes

--- src/main/java/com/pwc/bigchain/apigw/BigChainApiGatewayApplication.java
+++ src/main/java/com/pwc/bigchain/apigw/BigChainApiGatewayApplication.java
@@ -2,14 +2,16 @@ package com.pwc.bigchain.apigw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class BighainApiGatewayApplication {
+@EnableEurekaClient
+public class BigChainApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BighainApiGatewayApplication.class, args);
+		SpringApplication.run(BigChainApiGatewayApplication.class, args);
 	}
 	@Bean
 	public RestTemplate restTemplate()
--- src/test/java/com/pwc/bigchain/apigw/BigChainApiGatewayApplicationTests.java
+++ src/test/java/com/pwc/bigchain/apigw/BigChainApiGatewayApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BighainApiGatewayApplicationTests {
+public class BigChainApiGatewayApplicationTests {
 
 	@Test
 	public void contextLoads() {
