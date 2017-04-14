c3b88d43433d878cba659f524fe4adb48264a8f7
Lokesh <lokesh3440@gmail.com>
2017-04-14 21:46:33 +0800

    controller changes

--- src/main/java/com/pwc/bigchain/apigw/controller/ColleagueController.java
+++ src/main/java/com/pwc/bigchain/apigw/controller/ColleagueController.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bigchain.api.model.Colleague;
 import com.pwc.bigchain.apigw.service.CollegueManager;
